9c746f386d1cb2ae19e1167fa9afca35ac0c4077
Egor Artemyev <artemyev93@protonmail.com>
2022-07-22 00:00:29 +0300

    finalized tagging functionality

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, ShareView, SuccessView
+from .views import PostDetailView, ShareView, SuccessView, post_list
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    # path('', PostListView.as_view(), name='post_list'),
+    path('', post_list, name='post_list'),
+    path('tag/<slug:tag_slug>', post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          PostDetailView.as_view(), name='post_detail'),
     path('<int:post_id>/share/', ShareView.as_view(), name='post_share'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import View
@@ -9,12 +10,34 @@ from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
+# class PostListView(ListView):
+#
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 
 class PostDetailView(View):
